feb4d06023815a06fd6e01347efc7c1499fbb47e
huangjj <hujj1987@163.com>
2011-11-08 17:50:13 +0800

    添加高级检索画面

---
+++ 9PaoPao/ViewController/SearchNearbyViewController.m
@@ -0,0 +1,71 @@
+//
+//  SearchNearbyViewController.m
+//  9PaoPao
+//
+//  Created by huangjj on 11-11-8.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchNearbyViewController.h"
+
+
+@implementation SearchNearbyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImageView     *naviBackground = nil;
+    
+    naviBackground = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@""]];
+    
+    self.navigationItem.title = NSLocalizedString(@"SearchNearby Page Title", nil);
+    //[self.navigationController.navigationBar insertSubview: atIndex:0];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
