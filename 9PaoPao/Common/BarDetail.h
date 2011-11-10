de5f860638bfb84c9fd0c5799b54deb5aa0530dc
dondev <quanhongma.mail@gmail.com>
2011-11-10 19:06:59 +0800

    添加文件

---
+++ 9PaoPao/Common/BarDetail.h
@@ -0,0 +1,16 @@
+//
+//  BarDetail.h
+//  9PaoPao
+//
+//  Created by quanhong ma on 11/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface BarDetail : NSObject {
+    
+}
+
+@end
---
+++ 9PaoPao/Common/BarDetail.m
@@ -0,0 +1,14 @@
+//
+//  BarDetail.m
+//  9PaoPao
+//
+//  Created by quanhong ma on 11/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BarDetail.h"
+
+
+@implementation BarDetail
+
+@end
