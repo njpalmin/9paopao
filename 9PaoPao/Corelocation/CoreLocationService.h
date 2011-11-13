b7eb5a7eb57545a0fd20e23f2cf7898342ec58b5
maquanhong <maquanhong123@163.com>
2011-11-13 22:26:36 +0800

    增加corelocation.framework和mapkit.framework,corelocationservice.h,.m

---
+++ 9PaoPao/Corelocation/CoreLocationService.h
@@ -0,0 +1,13 @@
+//
+//  CoreLocationService.h
+//  9PaoPao
+//
+//  Created by  on 11/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreLocationService : NSObject
+
+@end
---
+++ 9PaoPao/Corelocation/CoreLocationService.m
@@ -0,0 +1,13 @@
+//
+//  CoreLocationService.m
+//  9PaoPao
+//
+//  Created by  on 11/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreLocationService.h"
+
+@implementation CoreLocationService
+
+@end
