2612649a801cd188928eb871e10803d0a583938d
maquanhong <maquanhong123@163.com>
2011-11-22 20:49:22 +0800

    添加酒吧评分界面

---
+++ 9PaoPao/ViewController/BarCommentDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BarCommentDetailViewController.h
+//  9PaoPao
+//
+//  Created by  on 11/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarCommentDetailViewController : UIViewController
+
+@end
