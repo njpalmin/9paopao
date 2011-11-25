00551f301ac5e857ead115f3d0bd03d1c308cad1
huangjj <hujj1987@163.com>
2011-11-25 14:56:57 +0800

    添加应用程序的icon

---
+++ 9PaoPao/ViewController/MyProfile/MyProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyProfileViewController.h
+//  9PaoPao
+//
+//  Created by huangjj on 11-11-25.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyProfileViewController : UIViewController {
+    
+}
+
+@end
