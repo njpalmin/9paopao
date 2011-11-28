ed269126c9925d1ec7cdaeae1db860a99de1b69b
maquanhong <maquanhong123@163.com>
2011-11-29 00:08:41 +0800

    添加品酒纪录

---
+++ 9PaoPao/ViewController/SocialFunction/DrinkWineRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrinkWineRecordViewController.h
+//  9PaoPao
+//
+//  Created by  on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinkWineRecordViewController : UIViewController
+
+@end
